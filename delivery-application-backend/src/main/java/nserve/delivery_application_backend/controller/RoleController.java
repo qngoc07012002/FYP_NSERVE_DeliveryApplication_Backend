fd57c53a32ef3f554192f43403debc45216fb707
Ngoc <quangngoc.07012002@gmail.com>
2024-08-13 22:36:42 +0700

    CRUD Role Controller

---
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/controller/RoleController.java
@@ -0,0 +1,42 @@
+package nserve.delivery_application_backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import nserve.delivery_application_backend.dto.request.RoleRequest;
+import nserve.delivery_application_backend.dto.response.ApiResponse;
+import nserve.delivery_application_backend.dto.response.RoleResponse;
+import nserve.delivery_application_backend.service.RoleService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/roles")
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
+@Slf4j
+public class RoleController {
+
+    RoleService roleService;
+
+    @PostMapping
+    ApiResponse<RoleResponse> create(@RequestBody RoleRequest request){
+        return ApiResponse.<RoleResponse>builder()
+                .result(roleService.create(request))
+                .build();
+    }
+
+    @GetMapping
+    ApiResponse<List<RoleResponse>> getAll(){
+        return ApiResponse.<List<RoleResponse>>builder()
+                .result(roleService.getAllRoles())
+                .build();
+    }
+
+    @DeleteMapping("/{role}")
+    ApiResponse<Void> delete(@PathVariable String role){
+        roleService.delete(role);
+        return ApiResponse.<Void>builder().build();
+    }
+}
