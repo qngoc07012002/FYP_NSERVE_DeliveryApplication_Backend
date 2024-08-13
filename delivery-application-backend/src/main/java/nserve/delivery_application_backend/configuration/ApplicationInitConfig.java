18ce7947022cd7ed02ae0723cce714ea9d00ec0b
Ngoc <quangngoc.07012002@gmail.com>
2024-08-13 22:43:29 +0700

    ApplicationInit Configure

---
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/configuration/ApplicationInitConfig.java
@@ -0,0 +1,47 @@
+package nserve.delivery_application_backend.configuration;
+
+import com.qngoc.identity_service.entity.Role;
+import com.qngoc.identity_service.entity.User;
+import com.qngoc.identity_service.repository.RoleRepository;
+import com.qngoc.identity_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+
+@Configuration
+@RequiredArgsConstructor // Lombok will generate a constructor with all the required fields
+@FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
+@Slf4j
+public class ApplicationInitConfig {
+
+    PasswordEncoder passwordEncoder;
+
+    @Bean
+    ApplicationRunner applicationRunner(UserRepository userRepository, RoleRepository roleRepository){
+        return args -> {
+            if (userRepository.findByUsername("admin").isEmpty()) {
+                Role role = new Role().builder()
+                        .name("ADMIN")
+                        .description("Admin role").build();
+                roleRepository.save(role);
+
+                var roles = new HashSet<Role>();
+                roles.add(role);
+                User user = User.builder()
+                        .username("admin")
+                        .password(passwordEncoder.encode("admin"))
+                         .roles(roles)
+                        .build();
+
+                userRepository.save(user);
+                log.warn("Admin user created with password: admin");
+            }
+        };
+    }
+}
---
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/enums/Role.java
@@ -0,0 +1,7 @@
+package nserve.delivery_application_backend.enums;
+
+public enum Role {
+    ADMIN,
+    USER
+}
+
