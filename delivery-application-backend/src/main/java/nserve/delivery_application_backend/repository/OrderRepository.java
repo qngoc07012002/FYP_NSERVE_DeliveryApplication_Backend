ca6169fa5c4122c94f72ffcdb8c60ccc4007e8a1
Ngoc <quangngoc.07012002@gmail.com>
2024-08-31 23:29:53 +0700

    Order Repository

---
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/repository/OrderRepository.java
@@ -0,0 +1,16 @@
+package nserve.delivery_application_backend.repository;
+
+import nserve.delivery_application_backend.entity.Order;
+import nserve.delivery_application_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, String> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+}
