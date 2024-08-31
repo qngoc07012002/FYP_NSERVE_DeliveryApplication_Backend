175950a86cc29e8989629a57e5c454c40bf368fa
Ngoc <quangngoc.07012002@gmail.com>
2024-08-31 23:29:35 +0700

    Review Repository

---
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/repository/ReviewRepository.java
@@ -0,0 +1,16 @@
+package nserve.delivery_application_backend.repository;
+
+import nserve.delivery_application_backend.entity.Review;
+import nserve.delivery_application_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, String> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+}
