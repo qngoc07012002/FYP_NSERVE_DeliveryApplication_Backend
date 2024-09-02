381b7a05a9abaa0e16e74fd34d6880881fec05c7
Ngoc <quangngoc.07012002@gmail.com>
2024-09-02 23:02:44 +0700

    Review Mapper

---
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/mapper/ReviewMapper.java
@@ -0,0 +1,19 @@
+package nserve.delivery_application_backend.mapper;
+
+import nserve.delivery_application_backend.dto.request.UserCreationRequest;
+import nserve.delivery_application_backend.dto.request.UserUpdateRequest;
+import nserve.delivery_application_backend.dto.response.UserResponse;
+import nserve.delivery_application_backend.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface ReviewMapper {
+    User toUser(UserCreationRequest request);
+
+    UserResponse toUserResponse(User user);
+
+    @Mapping(target = "roles", ignore = true)
+    void updateUser(@MappingTarget User user, UserUpdateRequest request);
+}
