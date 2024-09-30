044e30723e4119af85697e8f00a6edac2d57fff7
Ngoc <quangngoc.07012002@gmail.com>
2024-09-30 18:53:51 +0700

    SMS OTP Service

--- delivery-application-backend/src/main/java/nserve/delivery_application_backend/controller/AuthenticationController.java
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/controller/AuthenticationController.java
@@ -1,27 +1,31 @@
 package nserve.delivery_application_backend.controller;
 
 import com.nimbusds.jose.JOSEException;
+import com.twilio.Twilio;
+import com.twilio.rest.verify.v2.service.Verification;
+import com.twilio.rest.verify.v2.service.VerificationCheck;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import nserve.delivery_application_backend.dto.request.AuthenticationRequest;
-import nserve.delivery_application_backend.dto.request.IntrospectRequest;
-import nserve.delivery_application_backend.dto.request.LogoutRequest;
-import nserve.delivery_application_backend.dto.request.RefreshRequest;
+import lombok.extern.slf4j.Slf4j;
+import nserve.delivery_application_backend.dto.request.*;
 import nserve.delivery_application_backend.dto.response.ApiResponse;
 import nserve.delivery_application_backend.dto.response.AuthenticationResponse;
 import nserve.delivery_application_backend.dto.response.IntrospectResponse;
+import nserve.delivery_application_backend.dto.response.SMSResponse;
 import nserve.delivery_application_backend.service.AuthenticationService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
+@Slf4j
 public class AuthenticationController {
     AuthenticationService authenticationService;
 
@@ -34,7 +38,47 @@ public class AuthenticationController {
                 .build();
     }
 
-    
+    @PostMapping("/generateOTP")
+    ResponseEntity<String> sendSMS(@RequestBody SMSRequest request) {
+
+        Twilio.init("AC587ce38083646e33a9818d3ce6f3d6b7", "2b51292c820f170e88c18e28283b7735");
+
+        Verification verification = Verification.creator(
+                        "VA6a5cc2cac450c9e45350b856337ac19e", // this is your verification sid
+                        request.getPhoneNumber(),
+                        "sms")
+                .create();
+
+        System.out.println(verification.getStatus());
+
+        log.info("OTP has been successfully generated, and awaits your verification {}", LocalDateTime.now());
+
+        return new ResponseEntity<>("Message sent successfully", HttpStatus.OK);
+    }
+
+    @PostMapping("/verifyOTP")
+    ResponseEntity<String> verifyOTP(@RequestBody SMSRequest request) {
+
+        Twilio.init("AC587ce38083646e33a9818d3ce6f3d6b7", "2b51292c820f170e88c18e28283b7735");
+
+        try {
+
+            VerificationCheck verificationCheck = VerificationCheck.creator(
+                            "VA6a5cc2cac450c9e45350b856337ac19e", request.getOtp()) // pass verification SID here
+                    .setTo(request.getPhoneNumber())// pass generated OTP here
+                    .create();
+
+            if ("approved".equals(verificationCheck.getStatus())) {
+                return new ResponseEntity<>("Verification successful", HttpStatus.OK);
+            }
+            System.out.println(verificationCheck.getStatus());
+            return new ResponseEntity<>("Verification failed.", HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Verification failed.", HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
 
     @PostMapping("/introspect")
     ApiResponse<IntrospectResponse> authenticate(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
