d346b8de67cb72c473bba5ced9d96ffabdb712ad
Ngoc <quangngoc.07012002@gmail.com>
2024-10-21 23:05:56 +0700

    Restaurant Controller

--- delivery-application-backend/src/main/java/nserve/delivery_application_backend/controller/RestaurantController.java
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/controller/RestaurantController.java
@@ -3,8 +3,12 @@ package nserve.delivery_application_backend.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import nserve.delivery_application_backend.entity.Restaurant;
+import nserve.delivery_application_backend.service.RestaurantService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/restaurants")
@@ -12,4 +16,33 @@ import org.springframework.web.bind.annotation.RestController;
 @FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
 @Slf4j
 public class RestaurantController {
+    private RestaurantService restaurantService;
+
+    @GetMapping
+    public List<Restaurant> getAllRestaurants() {
+        return restaurantService.getAllRestaurants();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Restaurant> getRestaurantById(@PathVariable String id) {
+        return restaurantService.getRestaurantById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Restaurant createRestaurant(@RequestBody Restaurant restaurant) {
+        return restaurantService.createRestaurant(restaurant);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Restaurant> updateRestaurant(@PathVariable String id, @RequestBody Restaurant updatedRestaurant) {
+        return ResponseEntity.ok(restaurantService.updateRestaurant(id, updatedRestaurant));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRestaurant(@PathVariable String id) {
+        restaurantService.deleteRestaurant(id);
+        return ResponseEntity.noContent().build();
+    }
 }
