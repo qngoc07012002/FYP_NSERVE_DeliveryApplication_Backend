7e1a4e19da3aed819cb11ae825e10993ce3b3583
Ngoc <quangngoc.07012002@gmail.com>
2024-10-21 23:05:34 +0700

    Restaurant Service

--- delivery-application-backend/src/main/java/nserve/delivery_application_backend/service/RestaurantService.java
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/service/RestaurantService.java
@@ -3,12 +3,44 @@ package nserve.delivery_application_backend.service;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import nserve.delivery_application_backend.entity.Restaurant;
+import nserve.delivery_application_backend.repository.RestaurantRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor // Lombok will generate a constructor with all the required fields
 @FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
 @Slf4j
 public class RestaurantService {
+    private RestaurantRepository restaurantRepository;
 
+    public List<Restaurant> getAllRestaurants() {
+        return restaurantRepository.findAll();
+    }
+
+    public Optional<Restaurant> getRestaurantById(String id) {
+        return restaurantRepository.findById(id);
+    }
+
+    public Restaurant createRestaurant(Restaurant restaurant) {
+        return restaurantRepository.save(restaurant);
+    }
+
+    public Restaurant updateRestaurant(String id, Restaurant updatedRestaurant) {
+        return restaurantRepository.findById(id)
+                .map(restaurant -> {
+                    restaurant.setRestaurantName(updatedRestaurant.getRestaurantName());
+                    restaurant.setAddress(updatedRestaurant.getAddress());
+                    restaurant.setRating(updatedRestaurant.getRating());
+                    return restaurantRepository.save(restaurant);
+                })
+                .orElseThrow(() -> new RuntimeException("Restaurant not found with id: " + id));
+    }
+
+    public void deleteRestaurant(String id) {
+        restaurantRepository.deleteById(id);
+    }
 }
