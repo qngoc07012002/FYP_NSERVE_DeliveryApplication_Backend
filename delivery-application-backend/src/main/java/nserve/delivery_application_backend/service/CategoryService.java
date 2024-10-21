b2f8a20593226efd4e41b8d29494b3b09925f0fc
Ngoc <quangngoc.07012002@gmail.com>
2024-10-21 23:05:48 +0700

    Category Service

---
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/service/CategoryService.java
@@ -0,0 +1,60 @@
+package nserve.delivery_application_backend.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import nserve.delivery_application_backend.dto.request.UserCreationRequest;
+import nserve.delivery_application_backend.dto.request.UserUpdateRequest;
+import nserve.delivery_application_backend.dto.response.UserResponse;
+import nserve.delivery_application_backend.entity.Category;
+import nserve.delivery_application_backend.entity.User;
+import nserve.delivery_application_backend.enums.Role;
+import nserve.delivery_application_backend.exception.AppException;
+import nserve.delivery_application_backend.exception.ErrorCode;
+import nserve.delivery_application_backend.mapper.UserMapper;
+import nserve.delivery_application_backend.repository.CategoryRepository;
+import nserve.delivery_application_backend.repository.RoleRepository;
+import nserve.delivery_application_backend.repository.UserRepository;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor // Lombok will generate a constructor with all the required fields
+@FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
+@Slf4j
+public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> getCategoryById(String id) {
+        return categoryRepository.findById(id);
+    }
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(String id, Category updatedCategory) {
+        return categoryRepository.findById(id)
+                .map(category -> {
+                    category.setName(updatedCategory.getName());
+                    return categoryRepository.save(category);
+                })
+                .orElseThrow(() -> new RuntimeException("Category not found with id: " + id));
+    }
+
+    public void deleteCategory(String id) {
+        categoryRepository.deleteById(id);
+    }
+}
