27a8a5327265b4ee0b65418fe82bfe63cfb85f14
Ngoc <quangngoc.07012002@gmail.com>
2024-10-22 18:23:49 +0700

    Location Entity

---
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/entity/Location.java
@@ -0,0 +1,27 @@
+package nserve.delivery_application_backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    String Longitude;
+    String Latitude;
+
+
+
+}
