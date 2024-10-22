cc1e87c67ecc42bab63120f2cdaf911259e5a466
Ngoc <quangngoc.07012002@gmail.com>
2024-10-22 18:24:58 +0700

    Food Service

--- delivery-application-backend/src/main/java/nserve/delivery_application_backend/service/FoodService.java
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/service/FoodService.java
@@ -2,72 +2,109 @@ package nserve.delivery_application_backend.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.slf4j.Slf4j;
+
+import nserve.delivery_application_backend.dto.request.Food.FoodCreationRequest;
+import nserve.delivery_application_backend.dto.request.Food.FoodUpdateRequest;
+import nserve.delivery_application_backend.dto.response.FoodResponse;
 import nserve.delivery_application_backend.entity.Food;
+import nserve.delivery_application_backend.entity.Category;
+import nserve.delivery_application_backend.entity.Restaurant;
+import nserve.delivery_application_backend.exception.AppException;
+import nserve.delivery_application_backend.exception.ErrorCode;
+import nserve.delivery_application_backend.mapper.FoodMapper;
 import nserve.delivery_application_backend.repository.FoodRepository;
+import nserve.delivery_application_backend.repository.CategoryRepository;
+import nserve.delivery_application_backend.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor // Lombok will generate a constructor with all the required fields
+@RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
-@Slf4j
 public class FoodService {
 
-    private FoodRepository foodRepository;
+    FoodRepository foodRepository;
+    CategoryRepository categoryRepository;
+    RestaurantRepository restaurantRepository;
 
-    private final String imageDirectory = "src/main/resources/static/images/";
+    FoodMapper foodMapper;
 
-    public List<Food> getAllFoods() {
-        return foodRepository.findAll();
+    public List<FoodResponse> getAllFoods() {
+        return foodRepository.findAll().stream()
+                .map(this::convertToResponse)
+                .collect(Collectors.toList());
     }
 
-    public Optional<Food> getFoodById(String id) {
-        return foodRepository.findById(id);
+    public FoodResponse getFoodById(String id) {
+        Food food = foodRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorCode.FOOD_NOT_FOUND));
+        return convertToResponse(food);
     }
 
-    public Food createFood(Food food, MultipartFile image) throws IOException {
-        String imageUrl = saveImage(image);
-        food.setImgUrl(imageUrl);
-        return foodRepository.save(food);
+    public FoodResponse createFood(FoodCreationRequest foodRequest) throws IOException {
+        Category category = categoryRepository.findById(foodRequest.getCategoryId())
+                .orElseThrow(() -> new AppException(ErrorCode.CATEGORY_NOT_FOUND));
+        Restaurant restaurant = restaurantRepository.findById(foodRequest.getRestaurantId())
+                .orElseThrow(() -> new AppException(ErrorCode.RESTAURANT_NOT_FOUND));
+
+        Food food = new Food();
+        food.setName(foodRequest.getName());
+        food.setDescription(foodRequest.getDescription());
+        food.setPrice(foodRequest.getPrice());
+        food.setCategory(category);
+        food.setRestaurant(restaurant);
+        food.setImgUrl(foodRequest.getImgUrl());
+
+        Food savedFood = foodRepository.save(food);
+        return foodMapper.toFoodResponse(food);
     }
 
-    public Food updateFood(String id, Food foodDetails, MultipartFile image) throws IOException {
-        Food food = foodRepository.findById(id).orElseThrow(() -> new RuntimeException("Food not found"));
+    public FoodResponse updateFood(FoodUpdateRequest request) throws IOException {
+        Food food = foodRepository.findById(request.getId())
+                .orElseThrow(() -> new AppException(ErrorCode.FOOD_NOT_FOUND));
 
-        food.setName(foodDetails.getName());
-        food.setDescription(foodDetails.getDescription());
-        food.setPrice(foodDetails.getPrice());
-        food.setCategory(foodDetails.getCategory());
-        food.setRestaurant(foodDetails.getRestaurant());
+        food.setName(request.getName());
+        food.setDescription(request.getDescription());
+        food.setPrice(request.getPrice());
 
-        if (!image.isEmpty()) {
-            String imageUrl = saveImage(image);
-            food.setImgUrl(imageUrl);
+        Category category = categoryRepository.findById(request.getCategoryId())
+                .orElseThrow(() -> new AppException(ErrorCode.CATEGORY_NOT_FOUND));
+        Restaurant restaurant = restaurantRepository.findById(request.getRestaurantId())
+                .orElseThrow(() -> new AppException(ErrorCode.RESTAURANT_NOT_FOUND));
+
+        food.setCategory(category);
+        food.setRestaurant(restaurant);
+
+        if (request.getImgUrl() != null ) {
+            food.setImgUrl(request.getImgUrl());
         }
 
-        return foodRepository.save(food);
+        Food updatedFood = foodRepository.save(food);
+        return foodMapper.toFoodResponse(updatedFood);
     }
-
 
     public void deleteFood(String id) {
         foodRepository.deleteById(id);
     }
 
-    private String saveImage(MultipartFile image) throws IOException {
-        if (!image.isEmpty()) {
-            String fileName = UUID.randomUUID().toString() + "_" + image.getOriginalFilename();
-            File file = new File(imageDirectory + fileName);
-            image.transferTo(file);
-            return "/images/" + fileName;
-        }
-        return null;
+
+    private FoodResponse convertToResponse(Food food) {
+        return FoodResponse.builder()
+                .id(food.getId())
+                .name(food.getName())
+                .description(food.getDescription())
+                .price(food.getPrice())
+                .imgUrl(food.getImgUrl())
+                .category(food.getCategory())
+                .restaurant(food.getRestaurant())
+                .build();
     }
 }
