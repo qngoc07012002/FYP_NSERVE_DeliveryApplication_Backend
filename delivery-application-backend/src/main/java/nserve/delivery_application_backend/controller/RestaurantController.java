afda40f53c8cb0df130bd0720afb153673245a32
Ngoc <quangngoc.07012002@gmail.com>
2024-10-22 18:23:34 +0700

    Restaurant Controller

--- delivery-application-backend/src/main/java/nserve/delivery_application_backend/controller/RestaurantController.java
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/controller/RestaurantController.java
@@ -1,11 +1,15 @@
 package nserve.delivery_application_backend.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import nserve.delivery_application_backend.dto.request.Restaurant.RestaurantCreationRequest;
+import nserve.delivery_application_backend.dto.request.Restaurant.RestaurantUpdateRequest;
+import nserve.delivery_application_backend.dto.response.ApiResponse;
+import nserve.delivery_application_backend.dto.response.RestaurantResponse;
 import nserve.delivery_application_backend.entity.Restaurant;
 import nserve.delivery_application_backend.service.RestaurantService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,33 +20,33 @@ import java.util.List;
 @FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
 @Slf4j
 public class RestaurantController {
-    private RestaurantService restaurantService;
+    RestaurantService restaurantService;
 
-    @GetMapping
-    public List<Restaurant> getAllRestaurants() {
+    @PostMapping()
+    ApiResponse<RestaurantResponse> createRestaurant(@RequestBody @Valid RestaurantCreationRequest request) {
+        ApiResponse<RestaurantResponse> apiResponse = new ApiResponse<>();
+        apiResponse.setResult(restaurantService.createRestaurant(request));
+        return apiResponse;
+    }
+
+    @GetMapping()
+    List<RestaurantResponse> getAllRestaurants() {
         return restaurantService.getAllRestaurants();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Restaurant> getRestaurantById(@PathVariable String id) {
-        return restaurantService.getRestaurantById(id)
-                .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+    @GetMapping("/{restaurantId}")
+    RestaurantResponse getRestaurant(@PathVariable("restaurantId") String restaurantId) {
+        return restaurantService.getRestaurant(restaurantId);
     }
 
-    @PostMapping
-    public Restaurant createRestaurant(@RequestBody Restaurant restaurant) {
-        return restaurantService.createRestaurant(restaurant);
+    @PutMapping("/{restaurantId}")
+    RestaurantResponse updateRestaurant(@PathVariable("restaurantId") String restaurantId, @RequestBody RestaurantUpdateRequest request) {
+        return restaurantService.updateRestaurant(restaurantId, request);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Restaurant> updateRestaurant(@PathVariable String id, @RequestBody Restaurant updatedRestaurant) {
-        return ResponseEntity.ok(restaurantService.updateRestaurant(id, updatedRestaurant));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteRestaurant(@PathVariable String id) {
-        restaurantService.deleteRestaurant(id);
-        return ResponseEntity.noContent().build();
+    @DeleteMapping("/{restaurantId}")
+    String deleteRestaurant(@PathVariable("restaurantId") String restaurantId) {
+        restaurantService.deleteRestaurant(restaurantId);
+        return "Restaurant deleted";
     }
 }
