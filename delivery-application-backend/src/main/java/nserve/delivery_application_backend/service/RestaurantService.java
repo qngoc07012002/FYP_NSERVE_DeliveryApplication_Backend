d144a731c214b2bbbdbf9b7a58dbfecd1a75cd63
Ngoc <quangngoc.07012002@gmail.com>
2024-11-02 01:42:11 +0700

    Restaurant Service

--- delivery-application-backend/src/main/java/nserve/delivery_application_backend/service/RestaurantService.java
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/service/RestaurantService.java
@@ -3,6 +3,7 @@ package nserve.delivery_application_backend.service;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import nserve.delivery_application_backend.dto.request.Restaurant.RestaurantStatusUpdateRequest;
 import nserve.delivery_application_backend.dto.request.Restaurant.RestaurantUpdateRequest;
 import nserve.delivery_application_backend.dto.request.Restaurant.RestaurantCreationRequest;
 import nserve.delivery_application_backend.dto.response.RestaurantResponse;
@@ -13,9 +14,12 @@ import nserve.delivery_application_backend.exception.ErrorCode;
 import nserve.delivery_application_backend.mapper.RestaurantMapper; // Giả sử bạn có một mapper cho Restaurant
 import nserve.delivery_application_backend.repository.RestaurantRepository;
 import nserve.delivery_application_backend.repository.UserRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -32,17 +36,23 @@ public class RestaurantService {
         restaurant.setOwner(user);
 
         RestaurantResponse restaurantResponse = restaurantMapper.toRestaurantResponse(restaurantRepository.save(restaurant));
-        restaurantResponse.setOwner(restaurant.getOwner());
+
 
         return restaurantResponse;
     }
 
+    @PreAuthorize("hasRole('CUSTOMER')")
     public List<RestaurantResponse> getAllRestaurants() {
+
+
         return restaurantMapper.toRestaurantResponses(restaurantRepository.findAll());
     }
 
-    public RestaurantResponse getRestaurant(String restaurantId) {
-        Restaurant restaurant = restaurantRepository.findById(restaurantId)
+    public RestaurantResponse getRestaurant() {
+        var context = SecurityContextHolder.getContext();
+        String userId = context.getAuthentication().getName();
+
+        Restaurant restaurant = restaurantRepository.findByOwnerId(userId)
                 .orElseThrow(() -> new AppException(ErrorCode.RESTAURANT_NOT_FOUND));
         return restaurantMapper.toRestaurantResponse(restaurant);
     }
@@ -63,5 +73,22 @@ public class RestaurantService {
             throw new AppException(ErrorCode.RESTAURANT_NOT_FOUND);
         }
         restaurantRepository.deleteById(restaurantId);
+    }
+
+    public RestaurantResponse updateRestaurantStatus(String restaurantId, RestaurantStatusUpdateRequest statusUpdateRequest) {
+        Restaurant restaurant = restaurantRepository.findById(restaurantId)
+                .orElseThrow(() -> new AppException(ErrorCode.RESTAURANT_NOT_FOUND));
+
+        var context = SecurityContextHolder.getContext();
+        String userId = context.getAuthentication().getName();
+        log.info("userId: {}", userId);
+        log.info("restaurant owner id: {}", restaurant.getOwner().getId());
+        if (!userId.equals(restaurant.getOwner().getId())) {
+            throw new AppException(ErrorCode.UNAUTHORIZED);
+        }
+
+        restaurant.setStatus(statusUpdateRequest.getStatus());
+
+        return restaurantMapper.toRestaurantResponse(restaurantRepository.save(restaurant));
     }
 }
