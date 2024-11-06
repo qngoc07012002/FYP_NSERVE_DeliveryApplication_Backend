a45f316d5b72eb95aec341a44813f61093120349
Nguyen Quang Ngoc <quangngoc.07012002@gmail.com>
2024-11-06 08:50:36 +0700

    File Service

--- delivery-application-backend/src/main/java/nserve/delivery_application_backend/service/FileService.java
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/service/FileService.java
@@ -2,6 +2,8 @@ package nserve.delivery_application_backend.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import nserve.delivery_application_backend.dto.response.ApiResponse;
 import nserve.delivery_application_backend.dto.response.FileResponse;
 import nserve.delivery_application_backend.exception.AppException;
 import nserve.delivery_application_backend.exception.ErrorCode;
@@ -12,19 +14,21 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
 public class FileService {
 
+    CloudinaryService cloudinaryService;
+
     public FileResponse uploadFile(MultipartFile file) {
         String uploadDir = "src/main/resources/static/images/";
-
-
-        String contentType = file.getContentType();
-        if (!contentType.equals("image/png") && !contentType.equals("image/jpeg")) {
+        //String uploadDir = "uploads/";
+        if (!isImageFile(file)) {
             throw new AppException(ErrorCode.FILE_TYPE_INVALID);
         }
 
@@ -41,6 +45,25 @@ public class FileService {
             stream.write(file.getBytes());
             stream.close();
             return new FileResponse("/images/" + serverFile.getName());
+        } catch (Exception e) {
+            throw new AppException(ErrorCode.FILE_UPLOAD_FAILED);
+        }
+    }
+
+    private boolean isImageFile(MultipartFile file) {
+        String contentType = file.getContentType();
+        log.info("Content type: " + contentType);
+        return contentType != null && (contentType.startsWith("image/"));
+    }
+
+    public FileResponse uploadImage(MultipartFile file) {
+        if (!isImageFile(file)) {
+            throw new AppException(ErrorCode.FILE_TYPE_INVALID);
+        }
+
+        try {
+            String imgUrl = cloudinaryService.uploadImage(file);
+            return new FileResponse(imgUrl);
         } catch (Exception e) {
             throw new AppException(ErrorCode.FILE_UPLOAD_FAILED);
         }
