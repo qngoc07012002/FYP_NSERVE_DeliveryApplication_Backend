3e38e3bf967e1b138fe40771784ce7ef5e27cfce
Nguyen Quang Ngoc <quangngoc.07012002@gmail.com>
2024-11-06 08:50:20 +0700

    Order Item Service

--- delivery-application-backend/src/main/java/nserve/delivery_application_backend/service/OrderItemService.java
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/service/OrderItemService.java
@@ -6,11 +6,13 @@ import lombok.extern.slf4j.Slf4j;
 import nserve.delivery_application_backend.dto.request.UserCreationRequest;
 import nserve.delivery_application_backend.dto.request.UserUpdateRequest;
 import nserve.delivery_application_backend.dto.response.UserResponse;
+import nserve.delivery_application_backend.entity.OrderItem;
 import nserve.delivery_application_backend.entity.User;
 import nserve.delivery_application_backend.enums.Role;
 import nserve.delivery_application_backend.exception.AppException;
 import nserve.delivery_application_backend.exception.ErrorCode;
 import nserve.delivery_application_backend.mapper.UserMapper;
+import nserve.delivery_application_backend.repository.OrderItemRepository;
 import nserve.delivery_application_backend.repository.RoleRepository;
 import nserve.delivery_application_backend.repository.UserRepository;
 import org.springframework.security.access.prepost.PostAuthorize;
@@ -27,63 +29,9 @@ import java.util.List;
 @FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
 @Slf4j
 public class OrderItemService {
-    UserRepository userRepository;
-    UserMapper userMapper;
-    PasswordEncoder passwordEncoder;
-    RoleRepository roleRepository;
+   private final OrderItemRepository orderItemRepository;
 
-    public UserResponse createUser(UserCreationRequest request) {
-        if (userRepository.existsByEmail(request.getEmail())) {
-            throw new AppException(ErrorCode.USER_EXISTED);
-        }
-
-        User user = userMapper.toUser(request);
-        user.setPassword(passwordEncoder.encode(request.getPassword()));
-
-        HashSet<String> roles = new HashSet<>();
-        roles.add(Role.USER.name());
-
-
-        return userMapper.toUserResponse(userRepository.save(user));
-    }
-
-    @PreAuthorize("hasRole('ADMIN')")
-    public List<User> getAllUsers() {
-        log.info("In method get Users");
-        return userRepository.findAll();
-    }
-
-    public UserResponse getMyInfo(){
-        var context = SecurityContextHolder.getContext();
-        String email = context.getAuthentication().getName();
-
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
-
-        return userMapper.toUserResponse(user);
-    }
-
-    @PostAuthorize("returnObject.username == authentication.name")
-    public UserResponse getUser(String userId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
-
-        return userMapper.toUserResponse(user);
-    }
-
-    public UserResponse updateUser(String userId, UserUpdateRequest request){
-        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
-        System.out.println("User: " + user);
-        userMapper.updateUser(user, request);
-        user.setPassword(passwordEncoder.encode(request.getPassword()));
-
-        var roles = roleRepository.findAllById(request.getRoles());
-
-        user.setRoles(new HashSet<>(roles));
-
-
-        return userMapper.toUserResponse(userRepository.save(user));
-    }
-
-    public void deleteUser(String userId){
-        userRepository.deleteById(userId);
+    public List<OrderItem> findByOrderId(String id) {
+        return orderItemRepository.findByOrderId(id);
     }
 }
