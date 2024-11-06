467bf973d44a69e14c12134467a4c3bfd4d65667
Nguyen Quang Ngoc <quangngoc.07012002@gmail.com>
2024-11-06 08:50:30 +0700

    Order Service

--- delivery-application-backend/src/main/java/nserve/delivery_application_backend/service/OrderService.java
+++ delivery-application-backend/src/main/java/nserve/delivery_application_backend/service/OrderService.java
@@ -3,87 +3,74 @@ package nserve.delivery_application_backend.service;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import nserve.delivery_application_backend.dto.request.UserCreationRequest;
-import nserve.delivery_application_backend.dto.request.UserUpdateRequest;
-import nserve.delivery_application_backend.dto.response.UserResponse;
+import nserve.delivery_application_backend.dto.response.Order.OrderItemResponse;
+import nserve.delivery_application_backend.dto.response.Order.OrderRestaurantResponse;
+import nserve.delivery_application_backend.entity.Order;
+import nserve.delivery_application_backend.entity.Restaurant;
 import nserve.delivery_application_backend.entity.User;
-import nserve.delivery_application_backend.enums.Role;
 import nserve.delivery_application_backend.exception.AppException;
 import nserve.delivery_application_backend.exception.ErrorCode;
-import nserve.delivery_application_backend.mapper.UserMapper;
-import nserve.delivery_application_backend.repository.RoleRepository;
-import nserve.delivery_application_backend.repository.UserRepository;
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
+import nserve.delivery_application_backend.repository.*;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor // Lombok will generate a constructor with all the required fields
 @FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
 @Slf4j
 public class OrderService {
-    UserRepository userRepository;
-    UserMapper userMapper;
-    PasswordEncoder passwordEncoder;
-    RoleRepository roleRepository;
 
-    public UserResponse createUser(UserCreationRequest request) {
-        if (userRepository.existsByEmail(request.getEmail())) {
-            throw new AppException(ErrorCode.USER_EXISTED);
-        }
+    private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
+    private final RestaurantRepository restaurantRepository;
+    private final OrderItemService orderItemService;
 
-        User user = userMapper.toUser(request);
-        user.setPassword(passwordEncoder.encode(request.getPassword()));
-
-        HashSet<String> roles = new HashSet<>();
-        roles.add(Role.USER.name());
-
-
-        return userMapper.toUserResponse(userRepository.save(user));
-    }
-
-    @PreAuthorize("hasRole('ADMIN')")
-    public List<User> getAllUsers() {
-        log.info("In method get Users");
-        return userRepository.findAll();
-    }
-
-    public UserResponse getMyInfo(){
+    public List<OrderRestaurantResponse> getOrdersByRestaurant() {
         var context = SecurityContextHolder.getContext();
-        String email = context.getAuthentication().getName();
+        String userId = context.getAuthentication().getName();
 
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
 
-        return userMapper.toUserResponse(user);
+
+        Restaurant restaurant = restaurantRepository.findByOwnerId(user.getId())
+                .orElseThrow(() -> new AppException(ErrorCode.RESTAURANT_NOT_FOUND));
+
+
+        List<Order> orders = orderRepository.findByRestaurant(restaurant);
+
+        return orders.stream().map(this::convertToDTO).collect(Collectors.toList());
     }
 
-    @PostAuthorize("returnObject.username == authentication.name")
-    public UserResponse getUser(String userId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+    private OrderRestaurantResponse convertToDTO(Order order) {
+        OrderRestaurantResponse dto = new OrderRestaurantResponse();
+        dto.setId(order.getId());
+        dto.setOrderCode(order.getOrderCode());
+        dto.setCustomerName(order.getUser().getFullName());
+        dto.setDriverName(order.getDriver() != null ? order.getDriver().getUser().getFullName() : null);
+        dto.setDriverImgUrl(order.getDriver() != null ? order.getDriver().getUser().getImgUrl() : null);
+        dto.setStartLocation(order.getStartLocation().getAddress());
+        dto.setEndLocation(order.getEndLocation().getAddress());
+        dto.setCreateAt(order.getCreateAt());
+        dto.setTotalPrice(order.getTotalPrice());
+        dto.setOrderStatus(order.getOrderStatus());
 
-        return userMapper.toUserResponse(user);
-    }
-
-    public UserResponse updateUser(String userId, UserUpdateRequest request){
-        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
-        System.out.println("User: " + user);
-        userMapper.updateUser(user, request);
-        user.setPassword(passwordEncoder.encode(request.getPassword()));
-
-        var roles = roleRepository.findAllById(request.getRoles());
-
-        user.setRoles(new HashSet<>(roles));
+        List<OrderItemResponse> orderItems = orderItemService.findByOrderId(order.getId()).stream()
+                .map(item -> {
+                    OrderItemResponse itemDto = new OrderItemResponse();
+                    itemDto.setFoodId(item.getFood().getId());
+                    itemDto.setQuantity(item.getQuantity());
+                    itemDto.setTotalPrice(item.getPrice());
+                    itemDto.setFoodName(item.getFood().getName());
+                    return itemDto;
+                })
+                .toList();
 
 
-        return userMapper.toUserResponse(userRepository.save(user));
-    }
-
-    public void deleteUser(String userId){
-        userRepository.deleteById(userId);
+        dto.setOrderItems(orderItems);
+        return dto;
     }
 }
